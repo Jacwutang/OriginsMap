209e12b994dbf95e6df98c82b92cbda8d8d67cc7
Jack Wu <jackwu350@yahoo.com>
2018-01-14 12:05:20 -0800

    map zooms in and out with centering

--- visualization.js
+++ visualization.js
@@ -1,11 +1,18 @@
 
-
+var width = 1000;
+var height = 750;
 var svg = d3.select("#map")
           .append("svg")
-          .attr("height",600)
-          .attr("width", 900)
-          .append("g")
+          .attr("height",height)
+          .attr("width", width)
+          // .append("g")
           .attr("transform", "translate(30,30)")
+
+var g = svg.append("g")
+//            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+//            .append("g")
+//            .attr("id", "counties");
+
 
 
 d3.queue()
@@ -17,46 +24,52 @@ d3.queue()
 
 var projection = d3.geoAlbersUsa()
                    .translate([900/2,600/2])
-                   .scale(1000);
+                   .scale(1250);
 var path = d3.geoPath()
              .projection(projection);
+var centered;
+
+
 
 
 //Convert from TopoJSON into something
 
 function ready(error,data, file_states, players){
-  // console.log(data);
+
 
 
   var counties = topojson.feature(data,data.objects.counties).features;
-  console.log(counties);
-  var states = topojson.feature(file_states, file_states.objects.states).features;
-  console.log(states);
 
-  svg.selectAll(".county")
+  var states = topojson.feature(file_states, file_states.objects.states).features;
+
+
+ g.append('g')
+  .selectAll(".county")
   .data(counties)
   .enter().append("path")
   .attr("class", "county")
   .attr("d", path)
   .on('mouseover', function(d){
     d3.select(this).classed("selected", true)
-    
+
   })
   .on('mouseout', function(d){
     d3.select(this).classed("selected", false)
 
   })
+  .on('click',clicked);
 
 
 
-  svg.selectAll(".state")
-  .data(states)
-  .enter().append("path")
-  .attr("class","state")
-  .attr("d",path)
+
+  // svg.selectAll(".state")
+  // .data(states)
+  // .enter().append("path")
+  // .attr("class","state")
+  // .attr("d",path)
 
 
-  svg.selectAll(".city-circle")
+  g.selectAll(".city-circle")
      .data(players)
      .enter().append("circle")
      .attr("r",2)
@@ -79,8 +92,31 @@ function ready(error,data, file_states, players){
        }
      })
      .attr("opacity", 0.5)
+}
 
+function clicked(d) {
 
+  console.log(clicked);
+  var x, y, k;
 
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
 
+  g.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  g.transition()
+      .duration(750)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
 }
