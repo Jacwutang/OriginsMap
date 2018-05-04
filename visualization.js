fa8b5e029177d030a124205841c85b7569a6d133
Jack Wu <jackwu350@yahoo.com>
2018-05-03 18:16:31 -0700

    change font

--- visualization.js
+++ visualization.js
@@ -1,5 +1,3 @@
-// var Spinner = require('spin');
-// import {Spinner} from 'spin.js';
 
 var state_Hash = {
    "01": "Alabama",
@@ -55,28 +53,6 @@ var state_Hash = {
    "56": "Wyoming"
 }
 
-// var opts = {
-//   lines: 5, // The number of lines to draw
-//   length: 38, // The length of each line
-//   width: 17, // The line thickness
-//   radius: 45, // The radius of the inner circle
-//   scale: 1, // Scales overall size of the spinner
-//   corners: 1, // Corner roundness (0..1)
-//   color: '#ffffff', // CSS color or array of colors
-//   fadeColor: 'transparent', // CSS color or array of colors
-//   opacity: 0.25, // Opacity of the lines
-//   rotate: 0, // The rotation offset
-//   direction: 1, // 1: clockwise, -1: counterclockwise
-//   speed: 1, // Rounds per second
-//   trail: 60, // Afterglow percentage
-//   fps: 20, // Frames per second when using setTimeout() as a fallback in IE 9
-//   zIndex: 2e9, // The z-index (defaults to 2000000000)
-//   className: 'spinner', // The CSS class to assign to the spinner
-//   top: '50%', // Top position relative to parent
-//   left: '50%', // Left position relative to parent
-//   shadow: none, // Box-shadow for the lines
-//   position: 'absolute' // Element positioning
-// };
 
 
 var width = 1000;
